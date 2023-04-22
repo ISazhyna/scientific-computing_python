7409d8b263931ee21564661ebaddba8d26c6d22e
ISazhyna <seoirinas@gmail.com>
2023-04-21 19:21:37 -0500

    total time

--- add_time/main.py
+++ add_time/main.py
@@ -6,6 +6,15 @@ def add_time(start_time, duration, starting_day=None):
     # Parse duration
     duration_hours, duration_minutes = map(int, duration.split(':'))
 
-    return (start_time_hours, start_time_minutes)
+    # Total time
+    total_minutes = start_time_minutes + duration_minutes
+    total_hours = start_time_hours + duration_hours
+    if total_minutes>60:
+        total_minutes = total_minutes % 60
+        total_hours += 1
+    if am_pm == "PM":
+        total_hours += 12
 
-print(add_time("3:30 PM", "2:12"))
+    return (total_hours, total_minutes)
+
+print(add_time("3:30 PM", "33:35"))
