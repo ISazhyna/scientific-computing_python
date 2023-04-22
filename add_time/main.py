a64ac41b707b974e1f8dfbfe95090f8623d9db3f
ISazhyna <seoirinas@gmail.com>
2023-04-21 21:21:20 -0500

    add day of week

--- add_time/main.py
+++ add_time/main.py
@@ -18,12 +18,29 @@ def add_time(start_time, duration, starting_day=None):
     # Days later
     days_later = total_hours // 24
     if days_later == 1:
-        days_later_str = '(next day)'
+        days_later_str = f' (next day)'
     elif days_later > 1:
-        days_later_str = f'({days_later} days later)'
+        days_later_str = f' ({days_later} days later)'
     else:
         days_later_str = ''
 
-    return (total_hours, total_minutes, days_later_str)
+    # Define hours in AM PM period
+    total_hours = total_hours % 24
+    if total_hours > 12:
+        total_hours = total_hours % 12
+        am_pm = "PM"
+    else:
+        am_pm = "AM"
 
-print(add_time("3:30 PM", "33:35"))
+    # Add day of week if given
+    if starting_day:
+        starting_day = starting_day.lower()
+        weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+        start_weekday = weekdays.index(starting_day)
+        end_weekday = (start_weekday + days_later) % 7
+        day_of_week_str = f', {weekdays[end_weekday].capitalize()}'
+    else:
+        day_of_week_str = ''
+
+    full_str = f'{total_minutes}:{total_minutes:02d} {am_pm}{day_of_week_str}{days_later_str}'
+    return full_str
